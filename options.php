04b5b77e11933a1facde2df9f8f3b8458536bf22
Critick <andrew.stezenko@gmail.com>
2019-12-18 00:08:46 +0300

    1. Icons to fields added - can change in options. 2. Added options for show/hide form fields; checking fields in front-end.

--- options.php
+++ options.php
@@ -2,40 +2,148 @@
 	die( 'Forbidden' );
 }
 
-$options = array(
-	'send_to'	=> array(
+$options = [
+	'send_to'	=> [
     	'type'	=> 'text',
-    	'label'	=> __( 'Send E-mail To', 'mebel-laim' ),
-	    'desc'	=> __( 'Please enter e-mail for receiving messages', 'mebel-laim' )
-    ),
+    	'label'	=> esc_html__( 'Send E-mail To', 'mebel-laim' ),
+	    'desc'	=> esc_html__( 'Please enter e-mail for receiving messages', 'mebel-laim' )
+    ],
 
-    'legend'	=> array(
+    'legend'	=> [
     	'type'	=> 'text',
-    	'label'	=> __( 'Form Title', 'mebel-laim' ),
-	    'desc'	=> __( 'Please enter form title text', 'mebel-laim' )
-    ),
+    	'label'	=> esc_html__( 'Form Title', 'mebel-laim' ),
+	    'desc'	=> esc_html__( 'Please enter form title text', 'mebel-laim' )
+    ],
 
-    'firstname_placeholder'   => array(
-        'type'  => 'text',
-        'label' => __( 'First Name Placeholder', 'mebel-laim' ),
-        'desc'  => __( 'Please enter first name field placeholder text', 'mebel-laim' )
-    ),
+    'is_firstname_field'   => [
+        'type'  => 'multi-picker',
+        'label' => false,
+        'desc'  => false,
+        'value' => [
+            'firstname_choice'  => 'firstname_true'
+        ],
 
-    'phone_placeholder'   => array(
-        'type'  => 'text',
-        'label' => __( 'Phone Placeholder', 'mebel-laim' ),
-        'desc'  => __( 'Please enter phone field placeholder text', 'mebel-laim' )
-    ),
+        'picker'    => [
+            'firstname_choice'  => [
+                'type'      => 'select',
+                'label'     => esc_html__( 'Firstname Field', 'mebel-laim' ),
+                'desc'      => esc_html__( 'Please choose if form will have firstname field', 'mebel-laim' ),
+                'choices'   => [
+                    'firstname_true'    => esc_html__( 'Show Firstname Field', 'mebel-laim' ),
+                    'firstname_false'   => esc_html__( 'Hide Firstname Field', 'mebel-laim')
+                ]
+            ]
+        ],
 
-    'message_placeholder'   => array(
-        'type'  => 'text',
-        'label' => __( 'Message Placeholder', 'mebel-laim' ),
-        'desc'  => __( 'Please enter message field placeholder text', 'mebel-laim' )
-    ),
+        'choices'   => [
+            'firstname_true'    => [
+                'firstname_placeholder'   => [
+                    'type'  => 'text',
+                    'label' => esc_html__( 'First Name Placeholder', 'mebel-laim' ),
+                    'desc'  => esc_html__( 'Please enter first name field placeholder text', 'mebel-laim' )
+                ],
 
-    'button_text'	=> array(
+                'firstname_icon'  => [
+                    'type'          => 'icon-v2',
+                    'label'         => esc_html__( 'First Name Icon', 'mebel-laim' ),
+                    'desc'          => esc_html__( 'Please choose first name field icon', 'mebel-laim' ),
+                    'preview_size'  => 'medium',
+                    'modal_size'    => 'medium'
+                ]
+            ]
+        ],
+        'show_borders'  => false
+    ],
+
+    'is_phone_field'   => [
+        'type'  => 'multi-picker',
+        'label' => false,
+        'desc'  => false,
+        'value' => [
+            'phone_choice'  => 'phone_true'
+        ],
+
+        'picker'    => [
+            'phone_choice'  => [
+                'type'      => 'select',
+                'label'     => esc_html__( 'Phone Field', 'mebel-laim' ),
+                'desc'      => esc_html__( 'Please choose if form will have phone field', 'mebel-laim' ),
+                'choices'   => [
+                    'phone_true'    => esc_html__( 'Show Phone Field', 'mebel-laim' ),
+                    'phone_false'   => esc_html__( 'Hide Phone Field', 'mebel-laim')
+                ]
+            ]
+        ],
+
+        'choices'   => [
+            'phone_true'    => [
+                'phone_placeholder'   => [
+                    'type'  => 'text',
+                    'label' => esc_html__( 'Phone Placeholder', 'mebel-laim' ),
+                    'desc'  => esc_html__( 'Please enter phone field placeholder text', 'mebel-laim' )
+                ],
+
+                'phone_icon'  => [
+                    'type'          => 'icon-v2',
+                    'label'         => esc_html__( 'Phone Icon', 'mebel-laim' ),
+                    'desc'          => esc_html__( 'Please choose phone field icon', 'mebel-laim' ),
+                    'preview_size'  => 'medium',
+                    'modal_size'    => 'medium'
+                ]
+            ]
+        ],
+        'show_borders'  => false
+    ],
+
+    'is_message_field'   => [
+        'type'  => 'multi-picker',
+        'label' => false,
+        'desc'  => false,
+        'value' => [
+            'message_choice'  => 'message_true'
+        ],
+
+        'picker'    => [
+            'message_choice'  => [
+                'type'      => 'select',
+                'label'     => esc_html__( 'Message Field', 'mebel-laim' ),
+                'desc'      => esc_html__( 'Please choose if form will have message field', 'mebel-laim' ),
+                'choices'   => [
+                    'message_true'    => esc_html__( 'Show Message Field', 'mebel-laim' ),
+                    'message_false'   => esc_html__( 'Hide Message Field', 'mebel-laim')
+                ]
+            ]
+        ],
+
+        'choices'   => [
+            'message_true'    => [
+                'message_placeholder'   => [
+                    'type'  => 'text',
+                    'label' => esc_html__( 'Message Placeholder', 'mebel-laim' ),
+                    'desc'  => esc_html__( 'Please enter message field placeholder text', 'mebel-laim' )
+                ],
+
+                'message_icon'  => [
+                    'type'          => 'icon-v2',
+                    'label'         => esc_html__( 'Message Icon', 'mebel-laim' ),
+                    'desc'          => esc_html__( 'Please choose message field icon', 'mebel-laim' ),
+                    'preview_size'  => 'medium',
+                    'modal_size'    => 'medium'
+                ]
+            ]
+        ],
+        'show_borders'  => false
+    ],
+
+    'button_text'	=> [
     	'type'	=> 'text',
-    	'label'	=> __( 'Send Button Text', 'mebel-laim' ),
-	    'desc'	=> __( 'Please enter send button text', 'mebel-laim' )
-    )
-);
+    	'label'	=> esc_html__( 'Send Button Text', 'mebel-laim' ),
+	    'desc'	=> esc_html__( 'Please enter send button text', 'mebel-laim' )
+    ],
+
+    'popup_button_text'   => [
+        'type'  => 'text',
+        'label' => esc_html__( 'Success Popup Button Text', 'mebel-laim' ),
+        'desc'  => esc_html__( 'Please enter success popup button text', 'mebel-laim' )
+    ]
+];
--- views/view.php
+++ views/view.php
@@ -3,13 +3,67 @@ if ( !defined( 'FW' ) ) {
 	die( 'Forbidden' );
 }
 
+// Unyson Icons v2 option type frontend enqueue.
+fw()->backend->option_type('icon-v2')->packs_loader->enqueue_frontend_css();
 // Options from options.php file.
 $email_to = ( isset( $atts['send_to'] ) && $atts['send_to'] ) ? $atts['send_to'] : '';
 $legend = ( isset( $atts['legend'] ) && $atts['legend'] ) ? $atts['legend'] : '';
-$firstname_placeholder = ( isset( $atts['firstname_placeholder'] ) && $atts['firstname_placeholder'] ) ? $atts['firstname_placeholder'] : '';
-$phone_placeholder = ( isset( $atts['phone_placeholder'] ) && $atts['phone_placeholder'] ) ? $atts['phone_placeholder'] : '';
-$message_placeholder = ( isset( $atts['message_placeholder'] ) && $atts['message_placeholder'] ) ? $atts['message_placeholder'] : '';
 $button_text = ( isset( $atts['button_text'] ) && $atts['button_text'] ) ? $atts['button_text'] : 'Отправить';
+$popup_button_text = ( isset( $atts['popup_button_text'] ) && $atts['popup_button_text'] ) ? $atts['popup_button_text'] : 'OK';
+
+// Check is firstname field set to 'Show' or 'Hide' in options.php.
+switch ( $atts['is_firstname_field']['firstname_choice'] ) {
+	case 'firstname_true':
+		$firstname_placeholder = ( isset( $atts['is_firstname_field']['firstname_true']['firstname_placeholder'] ) &&
+								   $atts['is_firstname_field']['firstname_true']['firstname_placeholder'] ) ?
+								   $atts['is_firstname_field']['firstname_true']['firstname_placeholder'] :
+								   'Имя';
+		$firstname_icon = ( isset( $atts['is_firstname_field']['firstname_true']['firstname_icon'] ) &&
+								   $atts['is_firstname_field']['firstname_true']['firstname_icon'] ) ?
+								   $atts['is_firstname_field']['firstname_true']['firstname_icon']['icon-class'] :
+								   'fas fa-question';
+		break;
+
+	case 'firstname_false':
+		$firstname_placeholder = '';
+		break;
+}
+
+// Check is phone field set to 'Show' or 'Hide' in options.php.
+switch ( $atts['is_phone_field']['phone_choice'] ) {
+	case 'phone_true':
+		$phone_placeholder = ( isset( $atts['is_phone_field']['phone_true']['phone_placeholder'] ) &&
+							   $atts['is_phone_field']['phone_true']['phone_placeholder'] ) ?
+							   $atts['is_phone_field']['phone_true']['phone_placeholder'] :
+							   'Телефон';
+		$phone_icon = ( isset( $atts['is_phone_field']['phone_true']['phone_icon'] ) &&
+								   $atts['is_phone_field']['phone_true']['phone_icon'] ) ?
+								   $atts['is_phone_field']['phone_true']['phone_icon']['icon-class'] :
+								   'fas fa-question';
+		break;
+
+	case 'phone_false':
+		$phone_placeholder = '';
+		break;
+}
+
+// Check is message field set to 'Show' or 'Hide' in options.php.
+switch ( $atts['is_message_field']['message_choice'] ) {
+	case 'message_true':
+		$message_placeholder = ( isset( $atts['is_message_field']['message_true']['message_placeholder'] ) &&
+							   $atts['is_message_field']['message_true']['message_placeholder'] ) ?
+							   $atts['is_message_field']['message_true']['message_placeholder'] :
+							   'Сообщение';
+		$message_icon = ( isset( $atts['is_message_field']['message_true']['message_icon'] ) &&
+								   $atts['is_message_field']['message_true']['message_icon'] ) ?
+								   $atts['is_message_field']['message_true']['message_icon']['icon-class'] :
+								   'fas fa-question';
+		break;
+
+	case 'message_false':
+		$message_placeholder = '';
+		break;
+}
 ?>
 
 <!-- Form to send e-mail. -->
@@ -17,34 +71,51 @@ $button_text = ( isset( $atts['button_text'] ) && $atts['button_text'] ) ? $atts
 	<!-- All form fields. -->
 	<fieldset class = "cwpmf-fieldset">
 		<!-- Form title text. -->
-		<legend class = "cwpmf-legend"><?php printf( esc_html__( '%s', 'mebel-laim' ), $legend ) ?></legend>
+		<legend class = "cwpmf-legend">
+			<?php printf( esc_html__( '%s', 'mebel-laim' ), $legend ) ?>
+		</legend>
 
-		<!-- Label for first name field. -->
-		<label class = "cwpmf-label" for = "cwpmf-input-firstname">
-			<input id = "cwpmf-input-firstname" class = "cwpmf-input" type = "text" name = "cwpmf-input-firstname" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $firstname_placeholder ) ?>" />
-			<!-- Hidden field to show error if it occured. -->
-			<span class = "cwpmf-input-error cwpmf-input__firstname">
-				<span class = "cwpmf-input-error-msg"></span>
-			</span>
-		</label>
+		<!-- If firstname field is set to 'Show', so placeholder is not empty. -->
+		<?php if ( !empty( $firstname_placeholder ) ) : ?>
+			<label class = "cwpmf-label" for = "cwpmf-input-firstname">
+				<input id = "cwpmf-input-firstname" class = "cwpmf-input" type = "text" name = "cwpmf-input-firstname" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $firstname_placeholder ) ?>" />
+				<!-- Icon for this field. -->
+				<i class = "<?php echo esc_attr( $firstname_icon ) ?> cwpmf-icon"></i>
 
-		<!-- Label for phone number. -->
-		<label class = "cwpmf-label" for = "cwpmf-input-phone">
-			<input id = "cwpmf-input-phone" class = "cwpmf-input" type = "text" name = "cwpmf-input-phone" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $phone_placeholder ) ?>" />
-			<!-- Hidden field to show error if it occured. -->
-			<span class = "cwpmf-input-error cwpmf-input__phone">
-				<span class = "cwpmf-input-error-msg"></span>
-			</span>
-		</label>
+				<!-- Hidden field to show error if it occured. -->
+				<span class = "cwpmf-input-error cwpmf-input__firstname">
+					<span class = "cwpmf-input-error-msg"></span>
+				</span>
+			</label>
+		<?php endif ?>
 
-		<!-- Label for user message. -->
-		<label class = "cwpmf-label" for = "cwpmf-input-message">
-			<textarea id = "cwpmf-input-message" class = "cwpmf-textarea" name = "cwpmf-input-message" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $message_placeholder ) ?>"></textarea>
-			<!-- Hidden field to show error if it occured. -->
-			<span class = "cwpmf-input-error cwpmf-input__message">
-				<span class = "cwpmf-input-error-msg"></span>
-			</span>
-		</label>
+		<!-- If phone field is set to 'Show', so placeholder is not empty. -->
+		<?php if ( !empty( $firstname_placeholder ) ) : ?>
+			<label class = "cwpmf-label" for = "cwpmf-input-phone">
+				<input id = "cwpmf-input-phone" class = "cwpmf-input" type = "text" name = "cwpmf-input-phone" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $phone_placeholder ) ?>" />
+				<!-- Icon for this field. -->
+				<i class = "<?php echo esc_attr( $phone_icon ) ?> cwpmf-icon"></i>
+
+				<!-- Hidden field to show error if it occured. -->
+				<span class = "cwpmf-input-error cwpmf-input__phone">
+					<span class = "cwpmf-input-error-msg"></span>
+				</span>
+			</label>
+		<?php endif ?>
+
+		<!-- If message field is set to 'Show', so placeholder is not empty. -->
+		<?php if ( !empty( $firstname_placeholder ) ) : ?>
+			<label class = "cwpmf-label" for = "cwpmf-input-message">
+				<textarea id = "cwpmf-input-message" class = "cwpmf-textarea" name = "cwpmf-input-message" placeholder = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $message_placeholder ) ?>"></textarea>
+				<!-- Icon for this field. -->
+				<i class = "<?php echo esc_attr( $message_icon ) ?> cwpmf-icon"></i>
+
+				<!-- Hidden field to show error if it occured. -->
+				<span class = "cwpmf-input-error cwpmf-input__message">
+					<span class = "cwpmf-input-error-msg"></span>
+				</span>
+			</label>
+		<?php endif ?>
 
 		<!-- Button to submit form. -->
 		<button class = "button cwpmf-button" title = "<?php printf( esc_attr__( '%s', 'mebel-laim' ), $button_text ) ?>" form = "cwpmf" type = "submit">
@@ -67,7 +138,7 @@ $button_text = ( isset( $atts['button_text'] ) && $atts['button_text'] ) ? $atts
 		<!-- Popup button for better UI, I think. -->
 		<div class = "cwpmf-popup-button">
 			<a class = "button cwpmf-popup-button__link" href = "#">
-				<?php esc_html_e( 'OK', 'mebel-laim' ) ?>
+				<?php printf( esc_html__( '%s', 'mebel-laim' ), $popup_button_text ) ?>
 			</a>
 		</div>
 	</div>
